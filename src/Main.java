e4a29781b95e2ae2269647979ec9d19541cda6b9
BT_4N2_32 <animal@gmail.com>
2024-03-05 13:59:45 -0500

    Doesn't work yet

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,26 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         String[][] maze = MazeSolver.getMaze("src/Maze");
-        for (String[] row : maze) {
-            for (String column : row) {
-                System.out.print(column + " ");
-            }
-            System.out.println();
-        }
+        traverseMaze(maze);
+
     }
+
+    public static String[] traverseMaze(String[][] maze) {
+        String move = ".";
+        int rows = maze.length;
+        int columns = maze[0].length;
+        String[] path = new String[rows * columns];
+        for (int i = 0; i < rows; i++) {
+            for (int r = 0; r < columns; r++) {
+                if (maze[i][r].equals(move)) {
+                    path[i] = ("(" + i + ", " + r +  ")" + ", ");
+                }
+            }
+        }
+        for(int i = 0; i < path.length; i++){
+            System.out.print(path[i]);
+        }
+        return path;
+    }
+
 }
